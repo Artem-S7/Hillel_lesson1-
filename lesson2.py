e2862f3df5e97679842d1befc52ef08157993b55
Artem-S7/Hillel_lesson1 <asuslik0308@icliud.com>
2025-10-06 21:39:08 +0300

    Lesson_2

---
+++ lesson2.py
@@ -0,0 +1,19 @@
+print(4567 // 4567)
+print(4567 // 2283)
+print(4567 % 1141)
+print(4567 % 1521)
+
+number = 98765
+
+a = number // 10000
+b = (number // 1000) % 10
+c = (number // 100) % 10
+d = (number // 10) % 10
+e = number % 10
+
+reversed_number = e * 10000 + d * 1000 + c * 100 + b * 10 + a
+
+print(reversed_number)
+number = 4564
+
+
