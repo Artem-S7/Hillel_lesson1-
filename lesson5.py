1bd9b0e1bda9a6cf5e3f20c8282d736b45f85424
Artem-S7/Hillel_lesson1 <asuslik0308@icliud.com>
2025-10-18 21:17:55 +0300

    Lesson_5.1

---
+++ lesson5.py
@@ -0,0 +1,23 @@
+import string
+import keyword
+
+name = input("my_task: ")
+
+if not name:
+    print(False)
+elif name[0].isdigit():
+    print(False)
+elif name in keyword.kwlist:
+    print(False)
+elif name != name.lower():
+    print(False)
+elif any((ch in string.punctuation.replace("_", "")) or ch.isspace() for ch in name):
+    print(False)
+elif "__" in name:
+    print(False)
+elif name == "_":
+    print(True)
+elif name.startswith("__") and name.endswith("__"):
+    print(False)
+else:
+    print(True)
