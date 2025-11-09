1d27abd21468f5715b9f07f258905c0d5bd6042f
Artem-S7/Hillel_lesson1 <asuslik0308@icliud.com>
2025-11-09 12:16:04 +0200

    Lesson_11_2

--- lesson_11_2.py
+++ lesson_11_2.py
@@ -17,3 +17,6 @@ assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125,
 assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
 print("Ok")
 
+# test committed
+
+
